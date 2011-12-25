f2fc9d5260720782da62eb049918d8ded3faa574
Ales Justin <ales.justin@gmail.com>
2011-12-25 17:24:21 +0100

    Initial mains.

---
+++ agent/src/main/java/com/alterjoc/caliper/agent/Main.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright 2011-2013, KC CLASS, Robert Dukaric, Matej Lazar and Ales Justin.
+ */
+
+package com.alterjoc.caliper.agent;
+
+import java.lang.instrument.Instrumentation;
+
+/**
+ * Main agent class.
+ *
+ * @author <a href="mailto:ales.justin@jboss.org">Ales Justin</a>
+ */
+public class Main {
+    public static void premain(String[] args, Instrumentation inst) {
+        // TODO -- add ClassFileTransformer to inst
+    }
+}
---
+++ server/src/main/java/com/alterjoc/caliper/server/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright 2011-2013, KC CLASS, Robert Dukaric, Matej Lazar and Ales Justin.
+ */
+
+package com.alterjoc.caliper.server;
+
+/**
+ * Caliper server main.
+ *
+ * @author <a href="mailto:ales.justin@jboss.org">Ales Justin</a>
+ */
+public class Main {
+    public static void main(String[] args) {
+        // TODO
+    }
+}
