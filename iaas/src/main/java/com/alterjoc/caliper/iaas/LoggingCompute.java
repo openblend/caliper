1775c0e84a9ae0a81d330cdfae3303250d3fb663
Ales Justin <ales.justin@gmail.com>
2012-07-25 14:12:28 +0200

    Add logging Compute

---
+++ iaas/src/main/java/com/alterjoc/caliper/iaas/LoggingCompute.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2011-2013, KC CLASS; Matej Lazar, Ales Justin.
+ */
+
+package com.alterjoc.caliper.iaas;
+
+import java.util.Set;
+import java.util.logging.Logger;
+
+import org.jclouds.compute.RunNodesException;
+
+/**
+ * @author Ales Justin
+ */
+class LoggingCompute implements Compute {
+    private static final Logger log = Logger.getLogger(LoggingCompute.class.getName());
+    private Set<String> apps;
+
+    LoggingCompute(Set<String> apps) {
+        this.apps = apps;
+    }
+
+    public String createInstance() throws RunNodesException {
+        return "<noop>";
+    }
+
+    public String scaleUp() throws RunNodesException {
+        log.info("Scale up -- " + apps + " !!!");
+        return "<noop>";
+    }
+
+    public void scaleDown() throws Exception {
+        log.info("Scale down -- " + apps + " !!!");
+    }
+
+    public void terminate() {
+    }
+}
